8378b493a0d9d4d770e9ec157dbbf8132ef92867
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-11-26 18:56:47 +0300

    NEW: Basic style

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import Pokimons from "./components/Pokimons";
 
 function App() {
-  const pokemonNamesToFetch = [
-    "pikachu",
-    "charizard",
-    "bulbasaur",
-    "squirtle",
-    "jigglypuff",
-    "meowth",
-    "eevee",
-    "snorlax",
-    "mewtwo",
-    "gengar",
-  ];
-
-  const [pokimons, setPokimons] = useState([]);
-
-  useEffect(() => {
-    const dataFetcher = async () => {
-      const pokimonsData = await fetchPokimonsData();
-      setPokimons([...pokimonsData]);
-      console.log(pokimons);
-    };
-
-    dataFetcher();
-  }, []);
-
-  async function fetchPokimonsData() {
-    const pokimonsSet = new Set();
-
-    for (let pokimonName of pokemonNamesToFetch) {
-      try {
-        const pokimon = await fetchPokimonData(pokimonName);
-        pokimonsSet.add(pokimon);
-      } catch (error) {
-        console.error("Pokimon not found", error);
-      }
-    }
-
-    return pokimonsSet;
-  }
-
-  async function fetchPokimonData(pokimonName) {
-    try {
-      const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${pokimonName}`
-      );
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json();
-
-      const pokimon = {
-        name: data.name,
-        imgUrl: data.sprites.front_default,
-      };
-
-      return pokimon;
-    } catch (error) {
-      console.error("error fetching data", error);
-    }
-  }
+  return <Pokimons></Pokimons>;
 }
 
 export default App;
--- src/components/Pokimon.jsx
+++ src/components/Pokimon.jsx
@@ -1,8 +1,12 @@
+import "/src/styles/Pokimon.css";
+
 function Pokimon({ pokimon }) {
   return (
     <div className="pokimon">
-      <img src={pokimon.imgUrl} alt="" />
+      <img src={pokimon.imgUrl} alt="pokimon pitcure" />
       <div>{pokimon.name}</div>
     </div>
   );
 }
+
+export default Pokimon;
